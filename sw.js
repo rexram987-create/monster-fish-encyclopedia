47a90704dcc1a50582e2b253ff8010082f3c2cd3
rexram987-create <rexram987@gmail.com>
2026-09-15 07:09:26 +0300

    Bump PWA cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='monster-fish-v1';
+const CACHE='monster-fish-v2';
 const CORE=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
